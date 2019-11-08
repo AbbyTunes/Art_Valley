6c0015ecd4ef652a7b8313ab1e3c37c1a9651229
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-08 17:26:10 -0500

    Add icons

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -10,45 +10,50 @@ const Nav = props => {
   return (
     <div className="banner">
       <div className="nav-container">
-        <div className="nav-links">
-          <ApolloConsumer>
-            {client => (
-              <Query query={IS_LOGGED_IN}>
-                {({ data, loading, error }) => {
-                  if (loading) { return <p>loading...</p> }
-                  if (error) return `Error! ${error.message}`;
-                  if (data.isLoggedIn) {
-                    return (
-                      <div>
-                        <button
-                          onClick={e => {
-                            e.preventDefault();
-                            localStorage.removeItem("auth-token");
-                            client.writeData({ data: { isLoggedIn: false } });
-                            props.history.push("/");
-                          }} >
-                          Logout
-                        </button>
-                        <Link to="/">Home</Link>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div>
-                        <Link to="/">Home</Link>
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                      </div>
-                    );
-                  }
-                }}
-              </Query>
-            )}
-          </ApolloConsumer>
-        </div>
-        <div className="nav-logo">
-          <p>ArtValley</p>
-        </div>
+          <div className="nav-links">
+            <ApolloConsumer>
+              {client => (
+                <Query query={IS_LOGGED_IN}>
+                  {({ data, loading, error }) => {
+                    if (loading) { return <p>loading...</p> }
+                    if (error) return `Error! ${error.message}`;
+                    if (data.isLoggedIn) {
+                      return (
+                        <div>
+                          <button
+                            onClick={e => {
+                              e.preventDefault();
+                              localStorage.removeItem("auth-token");
+                              client.writeData({ data: { isLoggedIn: false } });
+                              props.history.push("/");
+                            }} >
+                            Logout
+                          </button>
+                          <Link to="/">Home</Link>
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <div>
+                          <Link to="/">Home</Link>
+                          <Link to="/login">Login</Link>
+                          <Link to="/register">Register</Link>
+                        </div>
+                      );
+                    }
+                  }}
+                </Query>
+              )}
+            </ApolloConsumer>
+          </div>
+          <div className="nav-icon-container">
+            <i class="fas fa-bars"></i>
+            <i className="fas fa-search"></i>
+            <i class="fas fa-indent"></i>
+          </div>
+          <div className="nav-logo">
+            ArtValley
+          </div>
       </div>
     </div>
   );
