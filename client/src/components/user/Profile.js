96d096c1121198e075fa7d5b91d08f15241a3d18
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-09 16:53:54 -0500

    Add user profile

---
+++ client/src/components/user/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./profile.css"
+
+const Profile = () => {
+    return(
+        <div className="temp">
+            hi lol
+        </div>
+    )
+}
+
+export default Profile;
