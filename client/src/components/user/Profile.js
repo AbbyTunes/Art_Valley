fb850a2e4ce1eb66a2b81428666ce55b0a63bdbf
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-09 19:07:44 -0500

    Style user profile

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Query } from "react-apollo";
 import "./profile.css"
+import { Link } from "react-router-dom";
 import Queries from "../../graphql/queries";
 const { FETCH_USER } = Queries;
+
 
 const Profile = (props) => {
     return (
@@ -10,18 +12,25 @@ const Profile = (props) => {
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error</p>;
-          
+
           let artList = data.user.likedArts.map(art => {
-            return <li>
+            return <li key={art.id}>
                 {art.title}
               </li>;
           })
           return (
-            <div className="temp">
-              <p>{data.user.name}</p>
-              <p>{data.user.email}</p>  
-              <div>
+            <div className="profile-container">
+              <div className="user-info">
+                <h1 className="user-header">{data.user.name}</h1>
+                <div className="settings-link">
+                  {/* <Link to="/settings">Settings</Link> */}
+                  <Link to="#">+</Link>
+                </div>
+              </div>
+              <div className="recently-liked">
+                <h1 className="profile-header">Recently Liked</h1>
                 {artList}
+                <Link to="#">See More</Link>
               </div>
             </div>
           );
