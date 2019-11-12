5a32b23c4b6e0930c20211d5eafd7cad933b45b3
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-12 16:50:50 -0500

    Get s3 to work

--- server/routes/fileUpload.js
+++ server/routes/fileUpload.js
@@ -15,14 +15,16 @@ var upload = multer({ storage: storage });
 
 
 router.post("/upload", upload.single("file"), function(req, res) {
+  console.log(req);
   const file = req.file;
-  const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
+  const s3FileURL = process.env.AWS_UPLOADED_FILE_URL_LINK;
 
   let s3bucket = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
   });
+
 
   var params = {
     Bucket: process.env.AWS_BUCKET_NAME,
@@ -33,6 +35,7 @@ router.post("/upload", upload.single("file"), function(req, res) {
   };
 
   s3bucket.upload(params, function(err, data) {
+    debugger;
     if (err) {
       res.status(500).json({ error: true, Message: err });
     } else {
@@ -42,7 +45,7 @@ router.post("/upload", upload.single("file"), function(req, res) {
         fileLink: s3FileURL + file.originalname,
         s3_key: params.Key
       };
-      var art = new Art(newFileUploaded);
+      var art = new Art({photoLink: newFileUploaded.fileLink, title: req.body.title, description: req.body.description, author: req.body.author, category: req.body.category});
       art.save(function(error, newFile) {
         if (error) {
           throw error;
