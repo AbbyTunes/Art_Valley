b661262951927e718bceba5151c9f5b728785d04
Abby Xu <abby.tsui2016@gmail.com>
2019-11-11 22:40:56 -0800

    art show page

--- client/src/components/art/art_show.js
+++ client/src/components/art/art_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
-import "./art_index.css"
-import { Link, withRouter } from "react-router-dom";
+import "./art_show.scss";
+import { withRouter } from "react-router-dom";
 import Queries from "../../graphql/queries";
 const { FETCH_ART } = Queries;
 
@@ -13,10 +13,59 @@ class ArtShow extends Component {
 
 	render() {
 		return (
-			<div clasName="art-container">Art Show</div>
+			<Query
+				query={FETCH_ART}
+				variables={{ artId: this.props.match.params.artId }} >
+				{({ loading, error, data }) => {
+					if (loading) return (
+						<div>
+							<p>Loading...</p>
+						</div>
+					);
+					if (error) return (
+						<div>
+							<p>Error</p>
+						</div>
+					);
+
+					console.dir(data)
+					const { author, description, photoLink, title, videoLink, likers, comments } = data.artById; 
+					return (
+            <div className="show-container">
+              <div className="show-art">
+								<div className="show-pic">
+									<img className="show-image" src={photoLink}></img>
+								</div>
+                
+                <div className="show-info">
+
+									<div className="info-main">
+										<div className="info-1">
+											<div className="show-title">{title}</div>
+											<div className="show-description">{description}</div>
+										</div>
+
+										<div className="info-2">
+											<div className="show-likes">Likes {likers.length}</div>
+										</div>
+									</div>
+                 
+									<div className="info-3">
+										<div className="show-comment">Comment</div>
+                  </div>
+                </div>
+
+                <div className="show-artist"></div>
+
+                <div className="show-category"></div>
+              </div>
+            </div>
+          );
+				}}
+			</Query>
+		
 		)
 	}
-
 }
 
 export default withRouter(ArtShow);
