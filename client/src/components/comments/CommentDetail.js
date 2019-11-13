dd37ca13e8c01a2281d43899004da8c572cc506d
CF <cfong7411@gmail.com>
2019-11-12 20:31:14 -0800

    Add styling for comments

--- client/src/components/comments/CommentDetail.js
+++ client/src/components/comments/CommentDetail.js
@@ -4,8 +4,23 @@ import "./comment.css";
 const CommentDetail = props => {
   return (
     <div className="comment-container">
-      <div className="comment-author">{props.comment.author.name}</div>
-      <div className="comment-body">{props.comment.body}</div>
+      <div className="comment-icon-container">
+        <div className="comment-author-icon">
+          <span
+            role="img"
+            alt=""
+            aria-label="debug"
+            className="comment-author-icon-pic"
+          >
+            {props.comment.author.name.slice(0, 1)}
+          </span>
+        </div>
+      </div>
+
+      <div className="comment-body-container">
+        <div className="comment-author">{props.comment.author.name}</div>
+        <div className="comment-body">{props.comment.body}</div>
+      </div>
     </div>
   );
 };
