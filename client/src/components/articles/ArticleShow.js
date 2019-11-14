10d13bb412a21c928a5279e306983aaf8bd7b302
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-13 23:44:06 -0500

    Add further article show styling

--- client/src/components/articles/ArticleShow.js
+++ client/src/components/articles/ArticleShow.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
-import "./article_show.scss";
+import "./article_show.css";
 // import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Queries from "../../graphql/queries";
@@ -20,12 +20,12 @@ class ArticleShow extends Component {
                 variables={{ _id: this.props.match.params.articleId }} >
                 {({ loading, error, data }) => {
                     if (loading) return (
-                        <div>
+                        <div className="temp">
                             <p>Loading...</p>
                         </div>
                     );
                     if (error) return (
-                        <div>
+                        <div className="temp">
                             <p>Error</p>
                         </div>
                     );
@@ -39,38 +39,43 @@ class ArticleShow extends Component {
                     const { body, photoLink, title, likers, comments, header } = data.article;
 
                     return (
-                        <div className="article-show-container">
-                            <div className="article-show-photo-container">
-                                <div className="article-show-photo">
-                                    <img className="article-show-image" src={photoLink}></img>
-                                </div>
-                                <div className="article-show-info">
-
-                                    <div className="article-info-main">
-                                        <div className="article-info-1">
-                                            <div className="article-show-title">{title}</div>
-                                            <div className="article-show-header">{header}</div>
-                                            <div className="article-show-body">{body}</div>
-                                        </div>
-
-                                        <div className="article-info-2">
-                                            <div className="article-show-likes">Likes {likers.length}</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="article-info-3">
-                                        <div className="article-show-comment">Comment</div>
-                                    </div>
-                                </div>
-
-                                <div className="article-show-artist">Add something</div>
-
-                                <div className="article-show-category"></div>
-                            </div>
-                            <div>
-                                <Link to="/community">Back to index</Link>
+                    <div className="article-show-container">
+                        <div className="article-show-photo-container">
+                            <div className="article-show-photo">
+                                <img
+                                    className="article-show-image"
+                                    src={photoLink}
+                                ></img>
                             </div>
                         </div>
+
+                        <div className="article-show-info">
+                            <div className="article-info-main">
+                                <div className="article-info-1">
+                                    <div className="article-show-title">{title}</div>
+                                    <div className="article-show-header">
+                                        {header}
+                                    </div>
+                                    <div className="article-show-body">{body}</div>
+                                </div>
+                                <div className="article-info-2">
+                                    <div className="article-show-likes">
+                                        Likes {likers.length}
+                                    </div>
+                                </div>
+                            </div>
+
+                          {/* <div className="article-info-3">
+                                        <div className="article-show-comment">Comment</div>
+                                </div> */}
+                        </div>
+
+                        <div>
+                            <Link to="/community" className="back-to-community">
+                                Back to Community
+                            </Link>
+                        </div>
+                    </div>
                     );
                 }}
             </Query>
