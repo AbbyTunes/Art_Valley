208ab283bdb6d28d42ec8e831a7fea4795d6cdf4
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-13 23:04:43 -0500

    Fix styling issue with width on article thumbnails

--- client/src/components/articles/ArticleIndexItem.js
+++ client/src/components/articles/ArticleIndexItem.js
@@ -3,28 +3,23 @@ import "./article-index-item.css";
 
 const ArticleIndexItem = (props) => {
     return (
-        <div className="article-index-item-container">
-            <div className="article-index-item-thumbnail-container">
-                <img src={props.article.photoLink}/>
-            </div>
-            <div className="article-index-item-author">
-                {props.article.author.name}
-            </div>
-            <div className="article-index-item-title">
-                {props.article.title}
-            </div>
-            <div className="article-index-item-header">
-                {props.article.header}
-            </div>
-            <div className="article-index-item-sub">
-                <div>
-                    {props.article.likers.length} likes
-                </div>
-                <div>
-                    {props.article.comments.length} comments
-                </div>
-            </div>
+      <div className="article-index-item-container">
+        <div className="article-index-item-thumbnail-container">
+          <img
+            src={props.article.photoLink}
+            className="article-index-item-thumbnail"
+          />
         </div>
+        <div className="article-index-item-author">
+          {props.article.author.name}
+        </div>
+        <div className="article-index-item-title">{props.article.title}</div>
+        <div className="article-index-item-header">{props.article.header}</div>
+        <div className="article-index-item-sub">
+          <div>{props.article.likers.length} likes</div>
+          <div>{props.article.comments.length} comments</div>
+        </div>
+      </div>
     );
 }
 
